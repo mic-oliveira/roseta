632c876916aedd349d38b3faa61a02554bfc4cda
Michael de Oliveira Ferreira <oliveiramichael@outlook.com>
2022-10-11 01:13:31 -0300

    first commit

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TranslateController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('translate', [\App\Http\Controllers\TranslateController::class, 'translation']);
+Route::post('translate', [TranslateController::class, 'translation']);
