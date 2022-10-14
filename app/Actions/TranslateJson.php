c11c06809c726af9bb0e7ae2aaffd2739b0e07ac
Michael de Oliveira Ferreira <oliveiramichael@outlook.com>
2022-10-14 02:14:04 -0300

    fix search translation

--- app/Actions/TranslateJson.php
+++ app/Actions/TranslateJson.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions;
 
+use Illuminate\Support\Str;
 use Lorisleiva\Actions\Concerns\AsAction;
 use Spatie\Browsershot\Browsershot;
 use Spatie\Crawler\Crawler;
@@ -12,6 +13,7 @@ class TranslateJson
     use AsAction;
 
     private string $to;
+    const SEPARATOR = ' || ';
 
     public function __construct(private Browsershot $browsershot)
     {
@@ -19,19 +21,19 @@ class TranslateJson
 
     public function handle($translate)
     {
-        $translated = [];
         $this->to = $translate['to'];
-        foreach ($translate['translate'] as $key => $value) {
-            $this->requestTranslation($translate['from'], $translate['to'], $value);
-            $translated[$key] = $this->filterHtmlBody($this->browsershot->bodyHtml())[0] ?? $value;
-        }
-        return $translated;
+        $toTranslate = $translate['translate'];
+        $this->requestTranslation($translate['from'], $translate['to'], array_values($toTranslate));
+        $translated = $this->filterHtmlBody($this->browsershot->bodyHtml());
+        return $this->parseResponse(array_keys($toTranslate), $translated);
     }
 
-    private function requestTranslation($from, $to, $word): void
+    private function requestTranslation($from, $to, $word)
     {
+
+        $s = urlencode(implode(self::SEPARATOR, $word));
         $this->browsershot
-            ->setUrl("https://translate.google.ca/?sl=${from}&tl=${to}&text=${word}&op=translate");
+            ->setUrl("https://translate.google.ca/?sl=${from}&tl=${to}&text=${s}&op=translate");
         $this->browsershot->noSandbox();
         $this->browsershot->setExtraNavigationHttpHeaders(["user-agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" ,
             'referer'=> 'https://www.google.com/']);
@@ -40,11 +42,20 @@ class TranslateJson
         $this->browsershot->delay(1000);
     }
 
-    private function filterHtmlBody(string $html) {
+    private function filterHtmlBody(string $html): string
+    {
         $translateTo = $this->to;
         $crawler = new DomCrawler($html);
-        return $crawler->filter('div span[lang="'.$translateTo.'"] span')->each(function(DomCrawler $node) {
-            return $node->first()->innerText();
-        });
+        return $crawler->filter('div span[lang="'.$translateTo.'"] span')->innerText();
+    }
+
+    private function parseResponse(array $keys, String $text): array
+    {
+        $parse = explode(self::SEPARATOR, $text);
+        $parsedTranslation = [];
+        foreach ($keys as $index => $key) {
+            $parsedTranslation[$key] =  $parse[$index];
+        }
+        return $parsedTranslation;
     }
 }
--- tests/Unit/TranlationTest.php
+++ tests/Unit/TranlationTest.php
@@ -7,7 +7,7 @@ test('translation from pt to de', function () {
     $pageContent = file_get_contents(__DIR__ . '/GoogleTradutor.html');
     $mockBrowsershot = Mockery::mock(Browsershot::class)->makePartial();
     $mockBrowsershot->expects('bodyHtml')->andReturn($pageContent);
-    $translate = new TranslateJson($mockBrowsershot, new Symfony\Component\DomCrawler\Crawler());
+    $translate = new TranslateJson($mockBrowsershot);
     $result = $translate->handle(['from' => 'pt-BR', "to" => 'de', 'translate' => ['test' => "teste"]]);
     dump($result);
     expect($result['test'])->toBe('Prüfung');
